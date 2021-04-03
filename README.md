736fe9a7eb8b2c1cc44f445c00867f63e619f2d6
Jaan Tollander de Balsch <jaan@hey.com>
2021-04-03 11:49:36 +0300

    Usage

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # CellListsBenchmarks.jl
 Benchmarks for `CellLists.jl` package.
+
+## Usage
+```julia
+using CellLists
+using CellListsBenchmarks
+
+n = 100
+d = 2
+r = 0.1
+seed = 1
+iterations = 1
+seconds = 1.0
+benchmark_algorithm(cell_list_serial, n, d, r, seed, iterations, seconds)
+benchmark_algorithm(cell_list_parallel, n, d, r, seed, iterations, seconds)
+benchmark_algorithm(brute_force, n, d, r, seed, iterations, seconds)
+benchmark_algorithm(cell_lists, n, d, r, seed, iterations, seconds)
+benchmark_near_neighbors(near_neighbors, n, d, r, seed, iterations, seconds)
+benchmark_near_neighbors(p_near_neighbors, n, d, r, seed, iterations, seconds)
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,16 @@ using Test
 using CellLists
 using CellListsBenchmarks
 
-benchmark_algorithm(cell_list_serial, 100, 2, 0.1, 1, 1, 1.0)
-benchmark_algorithm(cell_list_parallel, 100, 2, 0.1, 1, 1, 1.0)
-benchmark_algorithm(brute_force, 100, 2, 0.1, 1, 1, 1.0)
-benchmark_algorithm(cell_lists, 100, 2, 0.1, 1, 1, 1.0)
-benchmark_near_neighbors(near_neighbors, 100, 2, 0.1, 1, 2, 1.0)
-benchmark_near_neighbors(p_near_neighbors, 100, 2, 0.1, 1, 2, 1.0)
+n = 100
+d = 2
+r = 0.1
+seed = 1
+iterations = 1
+seconds = 1.0
+benchmark_algorithm(cell_list_serial, n, d, r, seed, iterations, seconds)
+benchmark_algorithm(cell_list_parallel, n, d, r, seed, iterations, seconds)
+benchmark_algorithm(brute_force, n, d, r, seed, iterations, seconds)
+benchmark_algorithm(cell_lists, n, d, r, seed, iterations, seconds)
+benchmark_near_neighbors(near_neighbors, n, d, r, seed, iterations, seconds)
+benchmark_near_neighbors(p_near_neighbors, n, d, r, seed, iterations, seconds)
 @test true
